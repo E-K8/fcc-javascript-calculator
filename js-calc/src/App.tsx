3c3306fe7509aaeed354c28913b73dd0d2507e80
E-K8 <ekatplat@gmail.com>
2024-01-12 22:06:10 +0000

    reflect changes in expression area for symbols

--- js-calc/src/App.tsx
+++ js-calc/src/App.tsx
@@ -1,9 +1,46 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const buttonPress = (symbol: string) => {
-    console.log(symbol);
+  const [answer, setAnswer] = useState('');
+  const [expression, setExpression] = useState('');
+  const et = expression.trim();
+
+  const isOperator = (symbol: string) => {
+    return /[*/+-]/.test(symbol);
   };
+
+  const buttonPress = (symbol: string) => {
+    if (symbol === 'clear') {
+      setAnswer('0');
+      setExpression('');
+    } else if (symbol === 'negative') {
+      if (answer === '') return;
+      setAnswer(
+        answer.toString().charAt(0) === '-' ? answer.slice(1) : '-' + answer
+      );
+    } else if (symbol === 'percentage') {
+      if (answer === '') return;
+      setAnswer((parseFloat(answer) / 100).toString());
+    } else if (isOperator(symbol)) {
+      setExpression(et + ' ' + symbol + ' ');
+    } else if (symbol === '=') {
+      calculate();
+    } else if (symbol === '0') {
+      if (expression.charAt(0) !== '0') {
+        setExpression(expression + symbol);
+      }
+    } else if (symbol === '.') {
+      // split by operators and get last number
+      const lastNumber = expression.split(/[-+/*]/g).pop();
+
+      // if the last number already has a decimal, don't add another
+      if (lastNumber?.includes('.')) return;
+      setExpression(expression + symbol);
+    }
+  };
+
+  const calculate = () => {};
 
   return (
     <>
@@ -11,8 +48,8 @@ function App() {
         <h1>JavaScript Calculator</h1>
         <div id='calculator'>
           <div id='display' style={{ textAlign: 'right' }}>
-            <div id='answer'>0</div>
-            <div id='expression'></div>
+            <div id='answer'>{answer}</div>
+            <div id='expression'>{expression}</div>
           </div>
 
           <button
@@ -66,7 +103,7 @@ function App() {
           </button>
           <button
             id='multiply'
-            onClick={() => buttonPress('multiply')}
+            onClick={() => buttonPress('*')}
             className='yellow'
           >
             *
@@ -94,7 +131,7 @@ function App() {
           </button>
           <button
             id='subtract'
-            onClick={() => buttonPress('subtract')}
+            onClick={() => buttonPress('-')}
             className='yellow'
           >
             -
@@ -120,11 +157,7 @@ function App() {
           >
             3
           </button>
-          <button
-            id='add'
-            onClick={() => buttonPress('add')}
-            className='yellow'
-          >
+          <button id='add' onClick={() => buttonPress('+')} className='yellow'>
             +
           </button>
           <button
